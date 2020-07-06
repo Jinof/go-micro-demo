3c8bfd769e44f4898b3816ef420c72eccc908e6d
Jinof <1474121785@qq.com>
2020-07-06 10:42:47 +0800

    :sparkles: Add casbin to auth.

--- gateway/main.go
+++ gateway/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/Jinof/go-micro-demo/pkg/plugins/login"
+	"github.com/Jinof/go-micro-demo/pkg/plugins/auth"
 	"github.com/micro/micro/v2/client/api"
 	"github.com/micro/micro/v2/cmd"
 )
@@ -11,7 +11,7 @@ func main() {
 }
 
 func init() {
-	if err := api.Register(login.NewPlugin()); err != nil {
+	if err := api.Register(auth.NewPlugin()); err != nil {
 		panic(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Jinof/go-micro-demo
 go 1.14
 
 require (
+	github.com/casbin/casbin/v2 v2.8.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/protobuf v1.4.2
 	github.com/micro/cli/v2 v2.1.2
