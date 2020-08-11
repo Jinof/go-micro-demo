628d7a1656d024a9a26ebc783cbb1924104b440e
Jinof <1474121785@qq.com>
2020-08-12 00:35:50 +0800

    :art: Rename user-srv to user_srv

---
+++ user_srv/api/handler/util.go
@@ -0,0 +1,24 @@
+package handler
+
+import "encoding/json"
+
+type respBody struct {
+	Status  int8        `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func ResponseBody(status int8, msg string, data interface{}) (string, error) {
+	body := respBody{
+		Status:  status,
+		Message: msg,
+		Data:    data,
+	}
+
+	b, err := json.Marshal(body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), nil
+}
---
+++ user_srv/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/Jinof/go-micro-demo/user_srv/api/handler"
+	"github.com/micro/go-micro/v2"
+	"log"
+)
+
+func main() {
+
+	service := micro.NewService(
+		micro.Name("go.micro.api.example"),
+	)
+
+	service.Init()
+
+	handler.RegisterHandler(service)
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
