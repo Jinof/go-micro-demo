05a9aa6f19c5566f5dea0143e65aa8a54f2516eb
Jinof <Jinof@foxmail.com>
2020-11-02 18:41:18 +0800

    :beers: Test trigger github actions

--- gateway/main.go
+++ gateway/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-    "github.com/Jinof/go-micro-demo/pkg/plugins/auth"
-    "github.com/micro/micro/v2/client/api"
-    "github.com/micro/micro/v2/cmd"
+	"github.com/Jinof/go-micro-demo/pkg/plugins/auth"
+	"github.com/micro/micro/v2/client/api"
+	"github.com/micro/micro/v2/cmd"
 )
 
 func main() {
-    cmd.Init()
+	cmd.Init()
 }
 
 func init() {
-    if err := api.Register(auth.NewPlugin()); err != nil {
-        panic(err)
-    }
+	if err := api.Register(auth.NewPlugin()); err != nil {
+		panic(err)
+	}
 }
+
+// Test trigger github actions
